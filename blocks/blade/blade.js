0cfd67a0c0e36686f6523b65485f9c06a84272dd
David Nuescheler <david@nuescheler.ch>
2023-02-20 09:54:05 -0800

    `cards` and `blade` blocks (#24)
    Issue #10
    
    Test URLs:
    
    Before: https://main--delta--hlxsites.hlx.page/
    After: https://cards-and-blade--delta--hlxsites.hlx.page/
    
    ---------
    
    Co-authored-by: Ashish Kayastha <akayastha@adobe.com>

---
+++ blocks/blade/blade.js
@@ -0,0 +1,13 @@
+export default function decorate(block) {
+  [...block.children].forEach((row) => {
+    [...row.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'blade-image';
+      else {
+        div.className = 'blade-body';
+        div.addEventListener('click', () => {
+          window.location.href = div.querySelector('a').href;
+        });
+      }
+    });
+  });
+}
