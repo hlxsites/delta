d48e0d88fee3d6148125723e1ef02999e28ada7d
Julien Ramboz <ramboz@users.noreply.github.com>
2023-02-23 17:18:14 -0800

    feat: add basic styles for skymiles pages (#39)
    Please always provide the [GitHub issue(s)](../issues) your PR is for,
    as well as test URLs where your change can be observed (before and
    after):
    
    Fix #<gh-issue-id>
    
    Test URLs:
    - Before: https://main--<repo>--<owner>.hlx.page/
    - After: https://<branch>--<repo>--<owner>.hlx.page/

--- us/en/skymiles/scripts/scripts.js
+++ us/en/skymiles/scripts/scripts.js
@@ -22,7 +22,7 @@ function buildHeroBlock(main) {
   // eslint-disable-next-line no-bitwise
   if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
     const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
+    section.append(buildBlock('hero', { elems: [picture] }));
     main.prepend(section);
   }
 }
