84a07fefbf15603868333e7eec3e945211f624f1
Ashish Kayastha <akayastha@adobe.com>
2023-03-03 18:56:22 -0600

    Fixed lint errors.

--- us/en/skymiles/blocks/toc/toc.js
+++ us/en/skymiles/blocks/toc/toc.js
@@ -1,12 +1,12 @@
 export default function decorate(block) {
-  const headings = document.querySelectorAll("h3");
-  const toc = document.createElement("ul");
+  const headings = document.querySelectorAll('h3');
+  const toc = document.createElement('ul');
 
   headings.forEach((heading) => {
-    const item = document.createElement("li");
-    const link = document.createElement("a");
-    
-    link.href = "#" + heading.id;
+    const item = document.createElement('li');
+    const link = document.createElement('a');
+
+    link.href = `#${heading.id}`;
     link.textContent = heading.textContent;
     item.appendChild(link);
     toc.appendChild(item);
