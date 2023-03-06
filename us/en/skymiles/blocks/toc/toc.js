ae1034883c1a886868bf66fdbb43c9063fdebe30
Ashish Kayastha <akayastha@adobe.com>
2023-03-06 12:40:17 -0600

    Fixed lint errors.

--- us/en/skymiles/blocks/toc/toc.js
+++ us/en/skymiles/blocks/toc/toc.js
@@ -1,12 +1,12 @@
 export default function decorate(block) {
   const currentToc = block.querySelector('ul');
   const newToc = document.createElement('ul');
-  
+
   [...currentToc.children].forEach((title) => {
     const textTags = document.querySelectorAll('h1, h2, h3, h4, h5, h6', 'p');
     let matchedTitle = null;
 
-    for (let i = 0; i < textTags.length; i++) {
+    for (let i = 0; i < textTags.length; i += 1) {
       if (textTags[i].textContent.includes(title.textContent)) {
         matchedTitle = textTags[i];
         break;
