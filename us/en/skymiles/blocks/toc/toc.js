3c08f9b7352da1fad29ff66f3699b0712aa85ea4
Ashish Kayastha <108121450+askayastha22@users.noreply.github.com>
2023-03-07 18:25:00 -0600

    feat: implement ToC block (#77)
    Fix #56 
    
    Test URLs:
    - Before:
    https://main--delta--hlxsites.hlx.page/drafts/alex/import/us/en/skymiles/airline-credit-cards/american-express-business-cards
    - After:
    https://issue-56--delta--hlxsites.hlx.page/drafts/alex/import/us/en/skymiles/airline-credit-cards/american-express-business-cards
    
    ---------
    
    Co-authored-by: Julien Ramboz <ramboz@users.noreply.github.com>

---
+++ us/en/skymiles/blocks/toc/toc.js
@@ -0,0 +1,29 @@
+export default function decorate(block) {
+  const currentToc = block.querySelector('ul');
+
+  if (currentToc) {
+    return;
+  }
+
+  const tocWrapper = document.querySelector('.toc-wrapper');
+  const toc = document.createElement('ul');
+
+  let nextSiblingDiv = tocWrapper.nextElementSibling;
+
+  while (nextSiblingDiv) {
+    const h2Tags = nextSiblingDiv.querySelectorAll('h2');
+
+    h2Tags.forEach((heading) => {
+      const item = document.createElement('li');
+      const link = document.createElement('a');
+
+      link.href = `#${heading.id}`;
+      link.textContent = heading.textContent;
+      item.appendChild(link);
+      toc.appendChild(item);
+    });
+
+    nextSiblingDiv = nextSiblingDiv.nextElementSibling;
+  }
+  block.innerHTML = toc.outerHTML;
+}
