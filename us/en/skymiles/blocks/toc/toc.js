ce1864d10ba28c3e53522e532243bef1fc905a17
Julien Ramboz <ramboz@adobe.com>
2023-03-09 07:54:12 -0800

    chore: fix linting issue

--- us/en/skymiles/blocks/toc/toc.js
+++ us/en/skymiles/blocks/toc/toc.js
@@ -5,7 +5,9 @@ export default function decorate(block) {
     return;
   }
   const toc = document.createElement('ul');
-  const h2Tags = [...document.querySelectorAll('h2')].filter((h2) => block.compareDocumentPosition(h2) & Node.DOCUMENT_POSITION_FOLLOWING);
+  const h2Tags = [...document.querySelectorAll('h2')]
+    // eslint-disable-next-line no-bitwise
+    .filter((h2) => block.compareDocumentPosition(h2) & Node.DOCUMENT_POSITION_FOLLOWING);
 
   h2Tags.forEach((heading) => {
     const item = document.createElement('li');
