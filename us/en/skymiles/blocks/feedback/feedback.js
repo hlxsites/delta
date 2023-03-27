dc5adf251294770022dbf0bcdf2e5447c0b02a74
Ashish Kayastha <akayastha@adobe.com>
2023-03-27 09:49:40 -0500

    Fixed lint errors.

--- us/en/skymiles/blocks/feedback/feedback.js
+++ us/en/skymiles/blocks/feedback/feedback.js
@@ -1,50 +1,50 @@
 export default async function decorate(block) {
   block.innerHTML = '';
-  const divSectionFeedback = document.createElement("div");
+  const divSectionFeedback = document.createElement('div');
 
-  const spanAckText = document.createElement("span");
-  spanAckText.setAttribute("id", "acknowledgeText");
-  spanAckText.textContent = "Thank you for your feedback.";
-  spanAckText.setAttribute("hidden", "true");
+  const spanAckText = document.createElement('span');
+  spanAckText.setAttribute('id', 'acknowledgeText');
+  spanAckText.textContent = 'Thank you for your feedback.';
+  spanAckText.setAttribute('hidden', 'true');
 
-  const inputSurveyID = document.createElement("input");
-  inputSurveyID.setAttribute("type", "hidden");
-  inputSurveyID.setAttribute("id", "surveyID");
-  inputSurveyID.setAttribute("name", "surveyID");
-  inputSurveyID.setAttribute("value", "Y0LLcnRhFy9KjxsjyPybemlqYF1spmda");
+  const inputSurveyID = document.createElement('input');
+  inputSurveyID.setAttribute('type', 'hidden');
+  inputSurveyID.setAttribute('id', 'surveyID');
+  inputSurveyID.setAttribute('name', 'surveyID');
+  inputSurveyID.setAttribute('value', 'Y0LLcnRhFy9KjxsjyPybemlqYF1spmda');
 
-  const divQuestionContainer = document.createElement("div");
-  divQuestionContainer.classList.add("question-container");
+  const divQuestionContainer = document.createElement('div');
+  divQuestionContainer.classList.add('question-container');
 
-  const spanAckQuestion = document.createElement("span");
-  spanAckQuestion.classList.add("acknowledge-question");
-  spanAckQuestion.textContent = "Did you find the information you were looking for on this page?";
+  const spanAckQuestion = document.createElement('span');
+  spanAckQuestion.classList.add('acknowledge-question');
+  spanAckQuestion.textContent = 'Did you find the information you were looking for on this page?';
 
-  const divFeedbackButtonsContainer = document.createElement("div");
-  divFeedbackButtonsContainer.classList.add("feedback-buttons-container");
+  const divFeedbackButtonsContainer = document.createElement('div');
+  divFeedbackButtonsContainer.classList.add('feedback-buttons-container');
 
   // Yes button
-  const divYesButton = document.createElement("div");
-  divYesButton.classList.add("feedback-button");
+  const divYesButton = document.createElement('div');
+  divYesButton.classList.add('feedback-button');
 
-  const yesButton = document.createElement("a");
-  yesButton.setAttribute("id", "yesBtn");
-  yesButton.classList.add("button", "primary");
-  yesButton.setAttribute("href", "javascript:void(0)");
-  yesButton.textContent = "Yes";
-  
+  const yesButton = document.createElement('a');
+  yesButton.setAttribute('id', 'yesBtn');
+  yesButton.classList.add('button', 'primary');
+  yesButton.setAttribute('href', 'javascript:void(0)');
+  yesButton.textContent = 'Yes';
+
   divYesButton.appendChild(yesButton);
 
   // No button
-  const divNoButton = document.createElement("div");
-  divNoButton.classList.add("feedback-button");
+  const divNoButton = document.createElement('div');
+  divNoButton.classList.add('feedback-button');
 
-  const noButton = document.createElement("a");
-  noButton.setAttribute("id", "noBtn");
-  noButton.classList.add("button", "primary");
-  noButton.setAttribute("href", "javascript:void(0)");
-  noButton.textContent = "No";
-  
+  const noButton = document.createElement('a');
+  noButton.setAttribute('id', 'noBtn');
+  noButton.classList.add('button', 'primary');
+  noButton.setAttribute('href', 'javascript:void(0)');
+  noButton.textContent = 'No';
+
   divNoButton.appendChild(noButton);
 
   divSectionFeedback.appendChild(spanAckText);
@@ -54,28 +54,28 @@ export default async function decorate(block) {
   divQuestionContainer.appendChild(divFeedbackButtonsContainer);
   divFeedbackButtonsContainer.appendChild(divYesButton);
   divFeedbackButtonsContainer.appendChild(divNoButton);
-  
+
   block.innerHTML = divSectionFeedback.innerHTML;
 
-  // Add click event listener to "Yes" button
-  document.getElementById("yesBtn").addEventListener("click", function() {
+  // Add click event listener to 'Yes' button
+  document.getElementById('yesBtn').addEventListener('click', () => {
     showAcknowledgeText();
   });
 
-  // Add click event listener to "No" button
-  document.getElementById("noBtn").addEventListener("click", function() {
+  // Add click event listener to 'No' button
+  document.getElementById('noBtn').addEventListener('click', () => {
     showAcknowledgeText();
   });
 
   // Add section class to fix the div width
-  const feedbackWrapper = document.querySelector(".feedback-wrapper");
-  feedbackWrapper.classList.add("section");
+  const feedbackWrapper = document.querySelector('.feedback-wrapper');
+  feedbackWrapper.classList.add('section');
 }
 
 function showAcknowledgeText() {
   // Replace question with acknowledge text
-  var acknowledgeText = document.getElementById("acknowledgeText");
-  var questionContainer = document.querySelector(".question-container");
-  questionContainer.setAttribute("hidden", "true");
-  acknowledgeText.removeAttribute("hidden");
+  var acknowledgeText = document.getElementById('acknowledgeText');
+  var questionContainer = document.querySelector('.question-container');
+  questionContainer.setAttribute('hidden', 'true');
+  acknowledgeText.removeAttribute('hidden');
 }
