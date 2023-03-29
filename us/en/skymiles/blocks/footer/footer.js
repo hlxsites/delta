2645df155d8b96b07a5f9026e4866b5617a6e616
asthabh23 <asthabhargava001@gmail.com>
2023-03-29 17:39:19 +0530

    chore: cleanup footer implementation

--- us/en/skymiles/blocks/footer/footer.js
+++ us/en/skymiles/blocks/footer/footer.js
@@ -34,6 +34,14 @@ export default async function decorate(block) {
     div.className = rowNames[i];
   });
 
+  const footerSocialInt = footer.querySelector('.footer-social-int');
+  const searchSocialInt = footerSocialInt.querySelectorAll('ul > li');
+  searchSocialInt.forEach((div) => {
+    if (div !== footerSocialInt.querySelector('ul > li:first-child')) {
+      div.className = 'color';
+    }
+  });
+
   const firstFooterDiv = footer.querySelector('.footer-links');
   firstFooterDiv.classList.add('footer-menu');
   const headings = firstFooterDiv.querySelectorAll('ul > li > strong');
@@ -48,27 +56,24 @@ export default async function decorate(block) {
 
   const menuHeadings = footer.querySelectorAll('.heading');
   const menuLists = footer.querySelectorAll('.menu');
-  menuHeadings.forEach((heading, index) => {
+  menuHeadings.forEach((heading) => {
     const caret = document.createElement('span');
     caret.classList.add('caret');
     caret.innerHTML = '&#8964;';
     heading.appendChild(caret);
     heading.addEventListener('click', () => {
-      const menuList = menuLists[index];
-      const isMenuListVisible = menuList.classList.contains('show');
-      menuLists.forEach((ul) => {
-        if (ul !== menuList && ul.classList.contains('show')) {
-          ul.classList.remove('show');
-          ul.previousElementSibling.querySelector('.caret').classList.remove('up');
-        }
-      });
-
-      if (!isMenuListVisible) {
-        menuList.classList.add('show');
-        caret.classList.add('up');
-      } else {
-        menuList.classList.remove('show');
-        caret.classList.remove('up');
+      const menuList = heading.nextElementSibling;
+      const isMenuListExpanded = menuList.getAttribute('aria-expanded') === 'true';
+      if (menuList && menuList.classList.contains('menu')) {
+        menuList.setAttribute('aria-expanded', 'false');
+        menuLists.forEach((ul) => {
+          if (ul !== menuList && ul.getAttribute('aria-expanded') === 'true') {
+            ul.setAttribute('aria-expanded', 'false');
+            ul.previousElementSibling.querySelector('.caret').classList.remove('up');
+          }
+        });
+        menuList.setAttribute('aria-expanded', !isMenuListExpanded);
+        caret.classList.toggle('up', !isMenuListExpanded);
       }
     });
   });
