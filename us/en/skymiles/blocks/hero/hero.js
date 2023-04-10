e5644b4859ec4369d9d53df335dd8d1001cce876
asthabh23 <asthabhargava001@gmail.com>
2023-04-10 16:29:30 +0530

    chore: use an h1 for the 1st large header on the page

--- us/en/skymiles/blocks/hero/hero.js
+++ us/en/skymiles/blocks/hero/hero.js
@@ -1,3 +1,15 @@
-export default async function decorate() {
-  // Nothing here yet
+export default async function decorate(block) {
+  const pStrong = block.querySelector('p strong');
+  const section = block.parentElement.parentElement.nextElementSibling;
+  const defaultContent = section.querySelector('div:first-child');
+  const pElement = document.createElement('p');
+  const strongElement = document.createElement('strong');
+  strongElement.textContent = pStrong.textContent;
+  pElement.appendChild(strongElement);
+  defaultContent.insertBefore(pElement, defaultContent.firstChild);
+
+  const pParentElement = pStrong.parentElement;
+  if (pParentElement && pParentElement.parentElement) {
+    pParentElement.parentElement.removeChild(pParentElement);
+  }
 }
