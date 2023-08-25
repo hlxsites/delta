2e2085de4a3540d59835029448c0788a81fc8d55
Vitaly Tsaplin <tsaplin@adobe.com>
2023-08-25 11:52:25 +0200

    feat: add image generation plugin

--- us/en/skymiles/scripts/scripts.js
+++ us/en/skymiles/scripts/scripts.js
@@ -386,12 +386,30 @@ async function loadPage() {
   await loadLazy(document);
   loadDelayed();
 
-  if (window.location.hostname.endsWith('hlx.page') || window.location.hostname === ('localhost')) {
-    const host = window.location.hostname === ('localhost') ? "http://localhost:4000" : "https://salmon-pebble-015589f10-dev.centralus.3.azurestaticapps.net";
+  if (window.location.hostname === ('localhost')) {
+
+    const url = "http://localhost:4000/static/js/bundle.js";
     const script = document.createElement("script");
-    script.setAttribute("src", `${host}/static/js/bundle.js`);
+    script.setAttribute("src", url);
     script.setAttribute("type", "text/javascript");
-    script.setAttribute("async", true);
+    script.setAttribute("async", "true");
+    script.crossOrigin = "anonymous";
+    document.head.appendChild(script);
+
+  } else if (window.location.hostname.endsWith('hlx.page')) {
+
+    const baseUrl = "https://salmon-pebble-015589f10-dev.centralus.3.azurestaticapps.net";
+
+    const link = document.createElement("link");
+    link.setAttribute("href", `${baseUrl}/static/css/main.css`);
+    link.setAttribute("rel", "stylesheet");
+    link.setAttribute("type", "text/css");
+    document.head.appendChild(link);
+
+    const script = document.createElement("script");
+    script.setAttribute("src", `${baseUrl}/static/js/main.js`);
+    script.setAttribute("type", "text/javascript");
+    script.setAttribute("async", "true");
     script.crossOrigin = "anonymous";
     document.head.appendChild(script);
   }
